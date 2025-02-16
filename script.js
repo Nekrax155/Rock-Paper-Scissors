8b7d1802ed4a212770e29690405fed60274bd692
Nekrax155 <190654254+Nekrax155@users.noreply.github.com>
2025-02-16 18:01:32 +0100

    Fix the syntax error

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getComputerChoice() {
     const choice = Math.random();
     console.log(choice);
-    if(choice <= 0,33){
+    if(choice <= 0.33){
             return "rock"; 
         }
         else if (choice <= 0.66 && choice > 0.33){
