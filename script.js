093d384628dc2c50fe24fc6796687923303d1d4d
Nekrax155 <190654254+Nekrax155@users.noreply.github.com>
2025-02-16 17:59:38 +0100

    Write the first function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello Console Test");
+function getComputerChoice() {
+    const choice = Math.random();
+    console.log(choice);
+    if(choice <= 0,33){
+            return "rock"; 
+        }
+        else if (choice <= 0.66 && choice > 0.33){
+            return "paper";
+        }      
+        else {
+            return "scissors";
+        } 
+}
+
