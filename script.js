b4986a9800c91aeaedf5069d78f570c115a3c385
Nekrax155 <190654254+Nekrax155@users.noreply.github.com>
2025-02-17 09:14:26 +0100

    Add logic to play a single round Add logic to play multiple rounds Add comment for the result of the round

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getComputerChoice() {
+/*function getComputerChoice() {
     const choice = Math.random();
     console.log(choice);
     if(choice <= 0.33){
@@ -12,3 +12,76 @@ function getComputerChoice() {
         } 
 }
 
+function getHumanChoice() {
+    let sign = window.prompt("Enter rock, paper or scissors");
+}
+
+let humanScore = 0;
+let ComputerScore = 0;
+
+function playRound (humanChoice, computerChoice ) {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    
+    if (humanChoice = "rock" )
+}*/
+
+    function getComputerChoice() {
+        const choice = Math.random();
+        if(choice <= 0.33){
+                return 1; 
+            }
+            else if (choice <= 0.66 && choice > 0.33){
+                return 2;
+            }      
+            else {
+                return 3;
+            } 
+    }
+    
+    function getHumanChoice() {
+        let sign = window.prompt("Enter rock, paper or scissors");
+        if(sign === "rock"){
+            return 1; 
+        }
+        else if (sign === "paper"){
+            return 2;
+        }      
+        else {
+            return 3;
+        } 
+    }
+    
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    function playRound (a, b) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        
+        if (humanChoice < computerChoice ){
+            if (humanChoice === 2 && computerChoice === 3 ){
+                ++computerScore;
+                return "You lose! Scissors beats Paper"  + " *** " + "Human: " + humanScore + " | " + "Computer " + computerScore;
+            }else if (humanChoice === 1 && computerChoice === 2 ){
+                ++computerScore;
+                return "You lose! Paper beats Rock"  + " *** " + "Human: " + humanScore + " | " + "Computer " + computerScore;}
+                else {
+                    ++humanScore;
+                    return "You win! Rock beats Scissors "  + " *** " + "Human: " + humanScore + " | " + "Computer " + computerScore;
+                }
+        }    
+        else if (humanChoice > computerChoice){
+            if (humanChoice === 3 && computerChoice === 2 ){
+                ++humanScore;
+                return "You win! Scissors beats Paper"  + " *** " + "Human: " + humanScore + " | " + "Computer " + computerScore;
+            }else if (humanChoice === 2 && computerChoice === 1 ){
+                ++humanScore;
+                return "You win! Paper beats Rock"  + " *** " + "Human: " + humanScore + " | " + "Computer " + computerScore;}
+                else {
+                    ++computerScore;
+                    return "You lose! Rock beats Scissors " + " *** " + "Human: " + humanScore + " | " + "Computer " + computerScore;
+                }
+            }
+        else {return "Tie!" + " *** " + "Human: " + humanScore + " | " + "Computer " + computerScore;}
+    }
